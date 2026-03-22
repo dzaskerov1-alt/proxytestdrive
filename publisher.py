8ebde872d53c274861b32f1f7578d820e47de1ab
dzaskerov1-alt <dzaskerov1@gmail.com>
2026-03-22 19:20:09 +0400

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,11 +1,11 @@
 async def send_result(bot, chat_id, server, port, secret, avg, rate, best=None):
     text = (
-        "✅ Хороший адрес\n\n"
-        f"Server: {server}\n"
-        f"Port: {port}\n"
-        f"Secret: {secret}\n\n"
-        f"Average latency: {int(avg)} ms\n"
-        f"Best latency: {int(best) if best is not None else '-'} ms\n"
-        f"Success rate: {int(rate * 100)}%"
+        "✅ Прошёл проверку\n\n"
+        f"🖥 Server: {server}\n"
+        f"🔌 Port: {port}\n"
+        f"🔑 Secret: {secret}\n\n"
+        f"📊 Average latency: {int(avg)} ms\n"
+        f"⚡ Best latency: {int(best) if best is not None else '-'} ms\n"
+        f"📈 Success rate: {int(rate * 100)}%"
     )
     await bot.send_message(chat_id=chat_id, text=text)
